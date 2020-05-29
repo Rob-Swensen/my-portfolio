93fc492a7a9d1b54372a99d8bc520b1a2a13550e
Rob-Swensen <rg.swensen801@gmail.com>
2020-05-29 12:31:40 -0600

    Contact.js progress

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,7 +3,11 @@ import React from 'react';
 function Contact(){
     return(
         <div>
-            Contact Component
+            <h2>I prefer to be contacted via e-mail or LinkeIn message.</h2>
+            <p>E-mail: rg.swensen801@gmail.com</p>
+            <a target='_blank' href='https://www.linkedin.com/in/robert-swensen-227287196/'>
+                <img src='https://i.pinimg.com/originals/ce/09/3c/ce093c7214ad357bb665cfd2f66a8b6b.png' alt='linkedIn logo'/>
+            </a>
         </div>
     )
 }
